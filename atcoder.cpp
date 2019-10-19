966b9a236712813d0a1449747d57072ab8d47101
arktan <gotupara@gmail.com>
2019-10-19 14:01:03 +0900

    191019

--- atcoder.cpp
+++ atcoder.cpp
@@ -74,59 +74,179 @@ typedef vector<string> VS;
 const int INF = 1e9;
 const int MOD = 1e9 + 7;
 // const int MOD = 998244353;
-const ll LINF = 5e18;
+const ll LINF = 1e18;
 // const double PI = atan(1.0) * 4.0;
 const ll dx[] = {1, 1, 0, -1, -1, -1, 0, 1};
 const ll dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
 #define PI 3.141592653589793238
 
-ll N, M;
-ll G[701][701];
-ll col[701];
-bool dp[701];
-ll cnum[2];
-bool bipartite = true;
+// 1000000007 で割ったあまりを扱う構造体
+template <int MOD> struct Fp {
+    long long val;
+    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+        if(val < 0) v += MOD;
+    }
+    constexpr int getmod() { return MOD; }
+    constexpr Fp operator-() const noexcept { return val ? MOD - val : 0; }
+    constexpr Fp operator+(const Fp &r) const noexcept {
+        return Fp(*this) += r;
+    }
+    constexpr Fp operator-(const Fp &r) const noexcept {
+        return Fp(*this) -= r;
+    }
+    constexpr Fp operator*(const Fp &r) const noexcept {
+        return Fp(*this) *= r;
+    }
+    constexpr Fp operator/(const Fp &r) const noexcept {
+        return Fp(*this) /= r;
+    }
+    constexpr Fp &operator+=(const Fp &r) noexcept {
+        val += r.val;
+        if(val >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Fp &operator-=(const Fp &r) noexcept {
+        val -= r.val;
+        if(val < 0) val += MOD;
+        return *this;
+    }
+    constexpr Fp &operator*=(const Fp &r) noexcept {
+        val = val * r.val % MOD;
+        return *this;
+    }
+    constexpr Fp &operator/=(const Fp &r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while(b) {
+            long long t = a / b;
+            a -= t * b;
+            swap(a, b);
+            u -= t * v;
+            swap(u, v);
+        }
+        val = val * u % MOD;
+        if(val < 0) val += MOD;
+        return *this;
+    }
+    constexpr bool operator==(const Fp &r) const noexcept {
+        return this->val == r.val;
+    }
+    constexpr bool operator!=(const Fp &r) const noexcept {
+        return this->val != r.val;
+    }
+    friend constexpr ostream &operator<<(ostream &os, const Fp<MOD> &x) noexcept {
+        return os << x.val;
+    }
+    friend constexpr istream &operator>>(istream &is, Fp<MOD> &x) noexcept {
+        return is >> x.val;
+    }
+    friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
+        if(n == 0) return 1;
+        auto t = modpow(a, n / 2);
+        t = t * t;
+        if(n & 1) t = t * a;
+        return t;
+    }
+};
 
-void dfs(ll now, ll c) {
-    col[now] = c;
-    cnum[c]++;
-    REP(to, N) {
-        if(to == now || !G[now][to]) continue;
-        if(col[to] == -1)
-            dfs(to, c ^ 1);
-        else if(col[to] != c ^ 1)
-            bipartite = false;
+using mint = Fp<MOD>;
+
+// size用
+struct UnionFind {
+    vector<ll> par;
+    vector<ll> sizes;
+
+    UnionFind(ll n) : par(n), sizes(n, 1) { REP(i, n) par[i] = i; }
+
+    ll find(ll x) {
+        if(x == par[x]) return x;
+        return par[x] = find(par[x]);
+    }
+
+    void unite(ll x, ll y) {
+        x = find(x);
+        y = find(y);
+
+        if(x == y) return;
+
+        if(sizes[x] < sizes[y]) swap(x, y);
+
+        par[y] = x;
+        sizes[x] += sizes[y];
+        sizes[y] = 0;
+    }
+
+    bool same(ll x, ll y) { return find(x) == find(y); }
+
+    ll size(ll x) { return sizes[find(x)]; }
+};
+
+struct edge {
+    ll from, to, cost;
+    bool operator<(const edge &another) const { return cost < another.cost; };
+    bool operator>(const edge &another) const { return cost > another.cost; };
+};
+vector<edge> es;
+
+ll N, M, X, dist[1010][1010];
+bool used[2020];
+vector<VPL> G(1010);
+
+ll kruskal() {
+    ll res = 0;
+    auto uf = UnionFind(N);
+    sort(All(es));
+    REP(i, SZ(es)) {
+        edge e = es[i];
+        if(!uf.same(e.from, e.to)) {
+            uf.unite(e.from, e.to);
+            G[e.from].emplace_back(e.to, e.cost);
+            G[e.to].emplace_back(e.from, e.cost);
+            used[i] = true;
+            res += e.cost;
+        }
+    }
+    return res;
+}
+
+void dfs(ll now, ll par, ll root, ll d) {
+    dist[root][now] = d;
+    FOREACH(e, G[now]) {
+        if(e.fi == par) continue;
+        dfs(e.fi, now, root, max(d, e.se));
     }
 }
 
 signed main() {
-    cin >> N >> M;
-    REP(i, N) REP(j, N) G[i][j] = 1;
+    cin >> N >> M >> X;
     REP(i, M) {
-        LCIN(A, B);
-        A--, B--;
-        G[A][B] = 0, G[B][A] = 0;
+        LCIN(u, v, w);
+        u--, v--;
+        es.push_back({u, v, w});
     }
-    Fill(col, -1);
-    VPL cnums;
-    REP(i, N) {
-        if(col[i] != -1) continue;
-        Fill(cnum, 0);
-        dfs(i, 0);
-        cnums.emplace_back(cnum[0], cnum[1]);
-        if(!bipartite) eputs(-1);
+    Fill(dist, 1LL << 30);
+    ll S = kruskal();
+    ll D = X - S;
+    REP(i, N) dfs(i, -1, i, 0);
+    ll large = 0, equal = 0, small = 0;
+    REP(i, M) {
+        if(used[i]) continue;
+        ll u = es[i].from, v = es[i].to;
+        ll dif = es[i].cost - dist[u][v];
+        if(dif < D)
+            small++;
+        else if(dif == D)
+            equal++;
+        else
+            large++;
     }
-    dp[0] = 1;
-    REP(i, cnums.size()) REPR(j, N) {
-        if(!dp[j]) continue;
-        dp[j] = 0;
-        dp[j + cnums[i].fi] = 1;
-        dp[j + cnums[i].se] = 1;
+    cout << S << " " << D << "\n";
+    cout << small << " " << equal << " " << large << "\n";
+    mint ans = 0;
+    if(D == 0) {
+        ans += (modpow((mint)2, N - 1) - 2) * modpow((mint)2, M - N + 1);
+        ans += (modpow((mint)2, equal) - 1) * modpow((mint)2, large) * 2;
+    } else if(D > 0) {
+        ans += (modpow((mint)2, equal) - 1) * modpow((mint)2, large) * 2;
     }
-    ll dif = N, left = -1;
-    REP(i, N + 1) {
-        if(!dp[i]) continue;
-        if(dif > abs(i - (N / 2))) dif = abs(i - (N / 2)), left = i;
-    }
-    cout << (left - 1) * left / 2 + (N - left - 1) * (N - left) / 2 << "\n";
+    cout << ans << "\n";
 }
