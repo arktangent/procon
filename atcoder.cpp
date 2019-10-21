904887d4430f738483ffe357d8ca2c0daf15a1ed
arktan <gotupara@gmail.com>
2019-10-22 00:51:15 +0900

    191022

--- atcoder.cpp
+++ atcoder.cpp
@@ -80,173 +80,26 @@ const ll dx[] = {1, 1, 0, -1, -1, -1, 0, 1};
 const ll dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
 #define PI 3.141592653589793238
 
-// 1000000007 で割ったあまりを扱う構造体
-template <int MOD> struct Fp {
-    long long val;
-    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
-        if(val < 0) v += MOD;
-    }
-    constexpr int getmod() { return MOD; }
-    constexpr Fp operator-() const noexcept { return val ? MOD - val : 0; }
-    constexpr Fp operator+(const Fp &r) const noexcept {
-        return Fp(*this) += r;
-    }
-    constexpr Fp operator-(const Fp &r) const noexcept {
-        return Fp(*this) -= r;
-    }
-    constexpr Fp operator*(const Fp &r) const noexcept {
-        return Fp(*this) *= r;
-    }
-    constexpr Fp operator/(const Fp &r) const noexcept {
-        return Fp(*this) /= r;
-    }
-    constexpr Fp &operator+=(const Fp &r) noexcept {
-        val += r.val;
-        if(val >= MOD) val -= MOD;
-        return *this;
-    }
-    constexpr Fp &operator-=(const Fp &r) noexcept {
-        val -= r.val;
-        if(val < 0) val += MOD;
-        return *this;
-    }
-    constexpr Fp &operator*=(const Fp &r) noexcept {
-        val = val * r.val % MOD;
-        return *this;
-    }
-    constexpr Fp &operator/=(const Fp &r) noexcept {
-        long long a = r.val, b = MOD, u = 1, v = 0;
-        while(b) {
-            long long t = a / b;
-            a -= t * b;
-            swap(a, b);
-            u -= t * v;
-            swap(u, v);
-        }
-        val = val * u % MOD;
-        if(val < 0) val += MOD;
-        return *this;
-    }
-    constexpr bool operator==(const Fp &r) const noexcept {
-        return this->val == r.val;
-    }
-    constexpr bool operator!=(const Fp &r) const noexcept {
-        return this->val != r.val;
-    }
-    friend constexpr ostream &operator<<(ostream &os, const Fp<MOD> &x) noexcept {
-        return os << x.val;
-    }
-    friend constexpr istream &operator>>(istream &is, Fp<MOD> &x) noexcept {
-        return is >> x.val;
-    }
-    friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
-        if(n == 0) return 1;
-        auto t = modpow(a, n / 2);
-        t = t * t;
-        if(n & 1) t = t * a;
-        return t;
-    }
-};
-
-using mint = Fp<MOD>;
-
-// size用
-struct UnionFind {
-    vector<ll> par;
-    vector<ll> sizes;
-
-    UnionFind(ll n) : par(n), sizes(n, 1) { REP(i, n) par[i] = i; }
-
-    ll find(ll x) {
-        if(x == par[x]) return x;
-        return par[x] = find(par[x]);
-    }
-
-    void unite(ll x, ll y) {
-        x = find(x);
-        y = find(y);
-
-        if(x == y) return;
-
-        if(sizes[x] < sizes[y]) swap(x, y);
-
-        par[y] = x;
-        sizes[x] += sizes[y];
-        sizes[y] = 0;
-    }
-
-    bool same(ll x, ll y) { return find(x) == find(y); }
-
-    ll size(ll x) { return sizes[find(x)]; }
-};
-
-struct edge {
-    ll from, to, cost;
-    bool operator<(const edge &another) const { return cost < another.cost; };
-    bool operator>(const edge &another) const { return cost > another.cost; };
-};
-vector<edge> es;
-
-ll N, M, X, dist[1010][1010];
-bool used[2020];
-vector<VPL> G(1010);
-
-ll kruskal() {
-    ll res = 0;
-    auto uf = UnionFind(N);
-    sort(All(es));
-    REP(i, SZ(es)) {
-        edge e = es[i];
-        if(!uf.same(e.from, e.to)) {
-            uf.unite(e.from, e.to);
-            G[e.from].emplace_back(e.to, e.cost);
-            G[e.to].emplace_back(e.from, e.cost);
-            used[i] = true;
-            res += e.cost;
-        }
-    }
-    return res;
-}
-
-void dfs(ll now, ll par, ll root, ll d) {
-    dist[root][now] = d;
-    FOREACH(e, G[now]) {
-        if(e.fi == par) continue;
-        dfs(e.fi, now, root, max(d, e.se));
-    }
-}
-
 signed main() {
-    cin >> N >> M >> X;
-    REP(i, M) {
-        LCIN(u, v, w);
-        u--, v--;
-        es.push_back({u, v, w});
+    LCIN(N);
+    VL S(1 << N);
+    VECCIN(S);
+    sort(All(S));
+    multiset<ll> lef;
+    REP(i, 1 << N) lef.emplace(S[i]);
+    VL now;
+    now.push_back(S[(1 << N) - 1]);
+    REP(i, N) {
+        VL tmp = now;
+        FOREACH(val, tmp) {
+            auto sel = lef.lower_bound(val);
+            if(sel == lef.begin()) eputs("No\n");
+            sel--;
+            now.emplace_back(*sel);
+            lef.erase(sel);
+        }
+        sort(rAll(now));
     }
-    Fill(dist, 1LL << 30);
-    ll S = kruskal();
-    ll D = X - S;
-    REP(i, N) dfs(i, -1, i, 0);
-    ll large = 0, equal = 0, small = 0;
-    REP(i, M) {
-        if(used[i]) continue;
-        ll u = es[i].from, v = es[i].to;
-        ll dif = es[i].cost - dist[u][v];
-        if(dif < D)
-            small++;
-        else if(dif == D)
-            equal++;
-        else
-            large++;
-    }
-    cout << S << " " << D << "\n";
-    cout << small << " " << equal << " " << large << "\n";
-    mint ans = 0;
-    if(D == 0) {
-        ans += (modpow((mint)2, N - 1) - 2) * modpow((mint)2, M - N + 1);
-        ans += (modpow((mint)2, equal) - 1) * modpow((mint)2, large) * 2;
-    } else if(D > 0) {
-        ans += (modpow((mint)2, equal) - 1) * modpow((mint)2, large) * 2;
-    }
-    cout << ans << "\n";
+    cout << "Yes"
+         << "\n";
 }
