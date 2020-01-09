e2c6ada30bbc1fa5fcb6b5871f4ab7eb1851e05a
arktan <gotupara@gmail.com>
2020-01-09 16:43:58 +0900

    200109update

--- atcoder.cpp
+++ atcoder.cpp
@@ -88,8 +88,7 @@ long long next_combination(long long sub) {
 template <typename F>
 #if defined(__has_cpp_attribute) && __has_cpp_attribute(nodiscard)
 [[nodiscard]]
-#elif defined(__GNUC__) &&                                                     \
-    (__GNUC__ > 3 || __GNUC__ == 3 && __GNUC_MINOR__ >= 4)
+#elif defined(__GNUC__) && (__GNUC__ > 3 || __GNUC__ == 3 && __GNUC_MINOR__ >= 4)
 __attribute__((warn_unused_result))
 #endif // defined(__has_cpp_attribute) && __has_cpp_attribute(nodiscard)
     static inline constexpr decltype(auto)
@@ -112,74 +111,111 @@ typedef vector<double> VD;
 typedef vector<string> VS;
 
 const int INF = 1e9;
-const int MOD = 1e9 + 7;
-// const int MOD = 998244353;
+// const int MOD = 1e9 + 7;
+const int MOD = 998244353;
 const ll LINF = 1e18;
 const ll dw[] = {1, 1, 0, -1, -1, -1, 0, 1};
 const ll dh[] = {0, 1, 1, 1, 0, -1, -1, -1};
 #define PI 3.141592653589793238
+#define EPS 1e-7
 
-// 0-indexed
-template <class T> struct BinaryIndexedTree {
-    vector<T> data;
-
-    BinaryIndexedTree(int sz) { data.assign(++sz, 0); }
-
-    T sum(int k) {
-        T ret = 0;
-        for(++k; k > 0; k -= k & -k) ret += data[k];
-        return (ret);
+// 1000000007 で割ったあまりを扱う構造体
+template <int MOD> struct Fp {
+    long long val;
+    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+        if(val < 0) v += MOD;
     }
-
-    void add(int k, T x) {
-        for(++k; k < data.size(); k += k & -k) data[k] += x;
+    constexpr int getmod() { return MOD; }
+    constexpr Fp operator-() const noexcept { return val ? MOD - val : 0; }
+    constexpr Fp operator+(const Fp &r) const noexcept {
+        return Fp(*this) += r;
     }
-};
-
-template <class T> struct AdditionalBIT : BinaryIndexedTree<T> {
-    int curr;
-
-    AdditionalBIT(int sz) : BinaryIndexedTree<T>(sz) {
-        curr = 1;
-        while(curr <= sz) curr <<= 1;
+    constexpr Fp operator-(const Fp &r) const noexcept {
+        return Fp(*this) -= r;
     }
-
-    int lower_bound(T w) {
-        if(w <= 0) return (0);
-        int i = 0;
-        for(int k = curr; k > 0; k >>= 1) {
-            if(i + k < BinaryIndexedTree<T>::data.size() &&
-               BinaryIndexedTree<T>::data[i + k] < w) {
-                w -= BinaryIndexedTree<T>::data[i + k];
-                i += k;
-            }
+    constexpr Fp operator*(const Fp &r) const noexcept {
+        return Fp(*this) *= r;
+    }
+    constexpr Fp operator/(const Fp &r) const noexcept {
+        return Fp(*this) /= r;
+    }
+    constexpr Fp &operator+=(const Fp &r) noexcept {
+        val += r.val;
+        if(val >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Fp &operator-=(const Fp &r) noexcept {
+        val -= r.val;
+        if(val < 0) val += MOD;
+        return *this;
+    }
+    constexpr Fp &operator*=(const Fp &r) noexcept {
+        val = val * r.val % MOD;
+        return *this;
+    }
+    constexpr Fp &operator/=(const Fp &r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while(b) {
+            long long t = a / b;
+            a -= t * b;
+            swap(a, b);
+            u -= t * v;
+            swap(u, v);
         }
-        return (i);
+        val = val * u % MOD;
+        if(val < 0) val += MOD;
+        return *this;
+    }
+    constexpr bool operator==(const Fp &r) const noexcept {
+        return this->val == r.val;
+    }
+    constexpr bool operator!=(const Fp &r) const noexcept {
+        return this->val != r.val;
+    }
+    friend constexpr ostream &operator<<(ostream &os, const Fp<MOD> &x) noexcept {
+        return os << x.val;
+    }
+    friend constexpr istream &operator>>(istream &is, Fp<MOD> &x) noexcept {
+        return is >> x.val;
+    }
+    friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
+        if(n == 0) return 1;
+        auto t = modpow(a, n / 2);
+        t = t * t;
+        if(n & 1) t = t * a;
+        return t;
     }
 };
+
+using mint = Fp<MOD>;
+
+const ll NMAX = 5e6;
+ll fac[NMAX + 1], inv[NMAX + 1], finv[NMAX + 1];
+
+void cominit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    FOR(i, 2, NMAX + 1) {
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+ll comb(ll n, ll k) {
+    if(n < k) return 0;
+    if(n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+ll hcomb(ll n, ll k) { return comb(k + n - 1, k); }
 
 signed main() {
-    LCIN(N);
-    VL B(N);
-    VECCIN(B);
-    MKORDER(N);
-    sort(All(od), [&](ll i, ll j) { return B[i] < B[j]; });
-    ll l = 0, r = 0;
-    AdditionalBIT<ll> lsum(N);
-    AdditionalBIT<ll> rsum(N);
-    ll ans = 0;
-    REP(i, N) {
-        ll idx = od[i];
-        ll now = idx + lsum.sum(N - 1) - lsum.sum(idx) - rsum.sum(idx);
-        if(now - l < N - 1 - r - now) {
-            ans += now - l;
-            lsum.add(idx, 1);
-            l++;
-        } else {
-            ans += N - 1 - r - now;
-            rsum.add(idx, 1);
-            r++;
-        }
-    }
+    LCIN(N, M);
+    cominit();
+    mint ans = hcomb(N, 3 * M);
+    ans -= (mint)N * hcomb(N, M - 1);
+    REP(i, M) { ans -= (mint)hcomb(N, i) * comb(N, 3 * M - 2 * i); }
     cout << ans << "\n";
 }
